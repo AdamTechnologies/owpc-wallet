98bf2e5b2b6db3c527b37d793ec9e3d0a7aa6d9c
manaf-abdul <abdulmanafp1996@gmail.com>
2023-10-22 22:46:36 +0530

    first commit

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,52 @@
+import { COLOR } from '@/constants/style'
+import { Avatar, Row } from '@nextui-org/react'
+import Image from 'next/image'
+import Link from 'next/link'
+
+export default function Navigation() {
+  return (
+    <Row justify="space-between" align="center">
+      <Link href="/" passHref>
+        <a className="navLink" data-testid="accounts">
+          <Image alt="accounts icon" src="/icons/accounts-icon.svg" width={27} height={27} />
+        </a>
+      </Link>
+
+      <Link href="/sessions" passHref>
+        <a className="navLink" data-testid="sessions">
+          <Image alt="sessions icon" src="/icons/sessions-icon.svg" width={27} height={27} />
+        </a>
+      </Link>
+
+      <Link href="/walletconnect" passHref>
+        <a className="navLink" data-testid="wc-connect">
+          <Avatar
+            size="lg"
+            css={{ cursor: 'pointer' }}
+            color={COLOR.yellow}
+            icon={
+              <Image
+                alt="wallet connect icon"
+                src="/owp-loyality.png"
+                width={30}
+                height={30}
+              />
+            }
+          />
+        </a>
+      </Link>
+
+      <Link href="/pairings" passHref>
+        <a className="navLink" data-testid="pairings">
+          <Image alt="pairings icon" src="/icons/pairings-icon.svg" width={25} height={25} />
+        </a>
+      </Link>
+
+      <Link href="/settings" passHref>
+        <a className="navLink" data-testid="settings">
+          <Image alt="settings icon" src="/icons/settings-icon.svg" width={27} height={27} />
+        </a>
+      </Link>
+    </Row>
+  )
+}
---
+++ src/constants/style.js
@@ -0,0 +1,7 @@
+export const COLOR = {
+    yellow: "#f0b90a",
+    lightYellow: `rgba(240, 185, 10, 0.27)`,
+    white:"white",
+    offwhite:"#6d6d6d",
+    black:"black"
+}
