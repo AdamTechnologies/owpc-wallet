a9b2aa78c503d72fbbfe211eb020bf8eaa96e7ea
manaf-abdul <abdulmanafp1996@gmail.com>
2023-10-30 22:19:41 +0530

    CHANGES

--- src/utils/MultiversxRequestHandlerUtil.ts
+++ src/utils/MultiversxRequestHandlerUtil.ts
@@ -1,6 +1,6 @@
 import { MULTIVERSX_SIGNING_METHODS } from '@/data/MultiversxData'
 import { getWalletAddressFromParams } from '@/utils/HelperUtil'
-import { multiversxAddresses, multiversxWallets } from '@/utils/MultiversxWalletUtil'
+// import { multiversxAddresses, multiversxWallets } from '@/utils/MultiversxWalletUtil'
 import { formatJsonRpcError, formatJsonRpcResult } from '@json-rpc-tools/utils'
 import { SignClientTypes } from '@walletconnect/types'
 import { getSdkError } from '@walletconnect/utils'
@@ -10,38 +10,38 @@ export async function approveMultiversxRequest(
 ) {
   const { params, id } = requestEvent
   const { request } = params
-  const account = getWalletAddressFromParams(multiversxAddresses, params)
-  const wallet = multiversxWallets[account]
+  // const account = getWalletAddressFromParams(multiversxAddresses, params)
+  // const wallet = multiversxWallets[account]
 
   switch (request.method) {
-    case MULTIVERSX_SIGNING_METHODS.MULTIVERSX_SIGN_MESSAGE:
-      const signedMessage = await wallet.signMessage(request.params.message)
-      return formatJsonRpcResult(id, signedMessage)
+    // case MULTIVERSX_SIGNING_METHODS.MULTIVERSX_SIGN_MESSAGE:
+    //   const signedMessage = await wallet.signMessage(request.params.message)
+    //   return formatJsonRpcResult(id, signedMessage)
 
-    case MULTIVERSX_SIGNING_METHODS.MULTIVERSX_SIGN_TRANSACTION:
-      const signTransaction = request.params.transaction
+    // case MULTIVERSX_SIGNING_METHODS.MULTIVERSX_SIGN_TRANSACTION:
+    //   const signTransaction = request.params.transaction
       // Transactions must be signed with the Sender's Private Key before submitting them to the MultiversX Network.
       // Signing is performed with the Ed25519 algorithm.
-      const signature = await wallet.signTransaction(signTransaction)
+      // const signature = await wallet.signTransaction(signTransaction)
 
-      return formatJsonRpcResult(id, signature)
+      // return formatJsonRpcResult(id, signature)
 
     case MULTIVERSX_SIGNING_METHODS.MULTIVERSX_SIGN_TRANSACTIONS:
       // MultiversX Allows for a Batch of Transactions to be signed
       const signTransactions = request.params.transactions
 
-      const signatures = await wallet.signTransactions(signTransactions)
-      return formatJsonRpcResult(id, signatures)
+      // const signatures = await wallet.signTransactions(signTransactions)
+      // return formatJsonRpcResult(id, signatures)
 
     case MULTIVERSX_SIGNING_METHODS.MULTIVERSX_SIGN_LOGIN_TOKEN:
     case MULTIVERSX_SIGNING_METHODS.MULTIVERSX_SIGN_NATIVE_AUTH_TOKEN:
       // Sometimes a dApp (and its backend) might want to reliably assign an off-chain user identity to a MultiversX address.
       // On this purpose, the signing providers allow a login token to be used within the login flow  - this token is signed using the wallet of the user.
       // Afterwards, a backend application would normally verify the signature of the token
-      const message = `${account}${request.params.token}`
-      const { signature: signedLoginToken } = await wallet.signMessage(message)
+      // const message = `${account}${request.params.token}`
+      // const { signature: signedLoginToken } = await wallet.signMessage(message)
 
-      return formatJsonRpcResult(id, { signature: signedLoginToken })
+      // return formatJsonRpcResult(id, { signature: signedLoginToken })
 
     default:
       throw new Error(getSdkError('UNSUPPORTED_METHODS').message)
