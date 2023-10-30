c5982d95b9be53c5ae7ae933175248d8b79c74db
manaf-abdul <abdulmanafp1996@gmail.com>
2023-10-30 23:41:22 +0530

    CHANGES

--- src/http/auth.tsx
+++ src/http/auth.tsx
@@ -1,6 +1,6 @@
 import axiosInstance from "@/utils/axios";
 
-export const LoginUserData = async (email: string, password: string) => {
+export const LoginUserData:any = async (email: string, password: string) => {
    console.log(email, password);
 
    const bodyFormData = { email: email, password: password };
