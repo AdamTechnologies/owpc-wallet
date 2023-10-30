f3ab0ac59f113356e92848413c961cdea035c949
manaf-abdul <abdulmanafp1996@gmail.com>
2023-10-30 21:25:37 +0530

    PWA

--- next.config.js
+++ next.config.js
@@ -1,11 +1,17 @@
-module.exports = {
+const withPWA = require('next-pwa');
+
+module.exports = withPWA({
+  pwa: {
+    dest: 'public', // The directory where the service worker and manifest will be output.
+    register: true,
+  },
   reactStrictMode: true,
   webpack(config) {
     config.resolve.fallback = {
       ...config.resolve.fallback,
-      fs: false
-    }
+      fs: false,
+    };
 
-    return config
-  }
-}
+    return config;
+  },
+});
