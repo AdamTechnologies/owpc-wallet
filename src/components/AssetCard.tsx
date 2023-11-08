4f5ff1f86f03ef6fdf60487a19d879ac2f7cfb69
manaf-abdul <abdulmanafp1996@gmail.com>
2023-11-08 12:32:06 +0530

    ASSETS-STYLE

--- src/components/AssetCard.tsx
+++ src/components/AssetCard.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import NextLink from 'next/link'
-import { Avatar, Card, Link, Text } from '@nextui-org/react'
+import { Avatar, Card, Col, Link, Row, Text } from '@nextui-org/react'
 import { formatBalance, formatMaticBalance, } from '@/utils/HelperUtil'
 import { COLOR } from '@/constants/style'
 
@@ -25,37 +25,44 @@ const AssetCard = ({ name, symbol, balance, token_address, decimals }: any) => {
                 bordered
                 borderWeight="light"
                 css={{
-                    position: "relative",
+                    // position: "relative",
                     marginBottom: "$6",
                     minHeight: "70px",
                 }}
                 data-testid={`session-card`}
             >
                 <Card.Body
-                    css={{
-                        flexDirection: "row",
-                        alignItems: "center",
-                        justifyContent: "space-between",
-                        overflow: "hidden",
-                    }}
+                // css={{
+                //     flexDirection: "column",
+                //     alignItems: "center",
+                //     justifyContent: "space-between",
+                //     overflow: "hidden",
+                // }}
                 >
-                    <div
-                        style={{
-                            position: "absolute",
-                            left: 0,
-                            top: 20,
-                        }}
-                    >
-                        <Text h5 css={{ marginLeft: "$12" }} color="warning">
-                            {symbol == "MATIC" ? formatMaticBalance(balance) : formatBalance(balance, decimals)}
-                        </Text>
-                    </div>
-                    <div style={{ flex: 1, marginLeft: "13rem" }}>
-                        <Text h5 css={{ marginLeft: "$9", color: COLOR.mediumYellow }}>{symbol}</Text>
-                        <Text weight="light" size={13} css={{ marginLeft: "$9", color: COLOR.lightYellow }}>
-                            {name}
-                        </Text>
-                    </div>
+                    {/* <div
+                        // style={{
+                        //     position: "absolute",
+                        //     left: 0,
+                        //     top: 20,
+                        // }}
+                    > */}
+
+                    <Row justify="space-around" align="center">
+                        <Col>
+                            <Text h5 css={{ marginLeft: "$1" }} color="warning">
+                                {symbol == "MATIC" ? formatMaticBalance(balance) : formatBalance(balance, decimals)}
+                            </Text>
+                        </Col>
+                        {/* </div> */}
+                        {/* <div style={{ flex: 1, marginLeft: "13rem" }}> */}
+                        <Col>
+                            <Text h5 css={{ marginLeft: "$1", color: COLOR.mediumYellow }}>{symbol}</Text>
+                            <Text weight="light" size={13} css={{ marginLeft: "$1", color: COLOR.lightYellow }}>
+                                {name}
+                            </Text>
+                        </Col>
+                        {/* </div> */}
+                    </Row>
                 </Card.Body>
             </Card>
         </NextLink>
